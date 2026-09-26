c8cd444343b8ccafdd2286f7c5e82f0162ea6d45
fleezappm-ux <fleeza.ppm@gmail.com>
2026-09-26 11:30:30 +0000

    Show correction requests on employee home and simplify shift cells

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -126,7 +126,7 @@ export function HomeView({
         <Button variant="outline" className="w-full mt-3 h-10 font-bold" onClick={onShowDashboard}>月の全体シフトを見る <ArrowRight className="w-4 h-4 ml-2" /></Button>
       </section>
 
-      <BulletinBoard compact periods={[{ label: boardMonthLabel, locked: boardLocked, requests }]} pendingCorrections={isEditor ? pendingCorrections : undefined} isEditor={isEditor} visibility={boardVisibility} correctionVisibility={correctionVisibility} operatorName={operatorName} onOpenBoard={onOpenBoard} />
+      <BulletinBoard compact periods={[{ label: boardMonthLabel, locked: boardLocked, requests }]} pendingCorrections={pendingCorrections} isEditor={isEditor} visibility={boardVisibility} correctionVisibility={correctionVisibility} operatorName={operatorName} onOpenBoard={onOpenBoard} />
 
       <button className="home-leave-request" onClick={onOpenLeaveRequest}>
         <CalendarDays className="w-5 h-5" /><div><strong>休み希望日を提出する</strong><span>希望受付中のシフト案に提出できます</span></div><ArrowRight className="w-5 h-5" />
